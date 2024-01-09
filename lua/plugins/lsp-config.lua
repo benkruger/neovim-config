d600920494d432a87441b28c0b62249c1215e632
Ben Kruger <ben@benkruger.com>
2024-01-09 10:36:45 -0800

    lsp configuration and telescope select box

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,8 +1,44 @@
 -- LSP Servers
--- https://github.com/williamboman/mason.nvim
 return {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
+  {
+    -- https://github.com/williamboman/mason.nvim
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    -- https://github.com/williamboman/mason-lspconfig.nvim
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "cssls",
+          "html",
+          "jsonls",
+          "lua_ls",
+          "marksman", -- Markdown
+          "ruby_ls",
+          "rubocop",
+          "sqls",
+          "tsserver" -- Javascript
+        }
+      })
+    end
+  },
+  {
+    -- https://github.com/neovim/nvim-lspconfig
+    "neovim/nvim-lspconfig",
+    -- Setup language servers.
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+      vim.keymap.set('n', 'K',  vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
 }
