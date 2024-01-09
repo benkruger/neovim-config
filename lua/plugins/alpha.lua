3a9a3e3ba14384acc29717937b474b9d4ee66f9d
Ben Kruger <ben@benkruger.com>
2024-01-09 11:45:42 -0800

    added greeter dashboard

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,11 @@
+-- https://github.com/goolord/alpha-nvim
+-- Greeter
+return {
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+}
